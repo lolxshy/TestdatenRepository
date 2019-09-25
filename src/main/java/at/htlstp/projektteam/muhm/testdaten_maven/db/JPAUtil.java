8eee4fdc3f540e492589d3b61e9185dd9e69180b
20140416 <20140416@IFTS2.htl-stp.if>
2019-09-25 09:25:11 +0200

    Entwicklung und Anpassung der JPAUtil Klasse

--- src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/db/JPAUtil.java
+++ src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/db/JPAUtil.java
@@ -3,14 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package at.htlstp.projektteam.muhm.testdaten_maven.db;
 
 /**
  *
  * @author Gabriel Kremsner
+ * @author Mateo Palac
  */
-public class JPAUtil {
-    
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class JPAUtil {
+
+    private static final EntityManagerFactory EMF;
+
+    static {
+        try {
+            EMF = Persistence.createEntityManagerFactory("at.htlstp.projektteam.muhm_Testdaten_maven_jar_1.0-SNAPSHOTPU");
+        } catch (Exception ex) {
+            System.err.println("EntityManagerFactory creation faild: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static EntityManagerFactory getEMF() {
+
+        return EMF;
+    }
+
+    public static void close() {
+        EMF.close();
+    }
 }
