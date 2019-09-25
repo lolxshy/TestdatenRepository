2d368c48bab6f4bf4a8aba4da4dde7cef161fd0f
20140064 <20140064@IFTS2.htl-stp.if>
2019-09-25 08:44:30 +0200

    Beziehungstabelle wurde für JPA angepasst

--- src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/app/model/ud_pt.java
+++ src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/app/model/ud_pt.java
@@ -10,26 +10,16 @@ package at.htlstp.projektteam.muhm.testdaten_maven.app.model;
  *
  * @author Gabriel Kremsner
  */
+import javax.persistence.OneToOne;
+
 public class ud_pt {
-    private int up_ud_id;
-    private String up_pt_kürzel;
+    @OneToOne
+    private UmfrageDaten up_ud;
+    @OneToOne
+    private Partei up_pt_kürzel;
     private int up_value;
 
-    public int getUp_ud_id() {
-        return up_ud_id;
-    }
-
-    public void setUp_ud_id(int up_ud_id) {
-        this.up_ud_id = up_ud_id;
-    }
-
-    public String getUp_pt_kürzel() {
-        return up_pt_kürzel;
-    }
-
-    public void setUp_pt_kürzel(String up_pt_kürzel) {
-        this.up_pt_kürzel = up_pt_kürzel;
-    }
+  
 
     public int getUp_value() {
         return up_value;
@@ -42,15 +32,15 @@ public class ud_pt {
     public ud_pt() {
     }
 
-    public ud_pt(int up_ud_id, String up_pt_id, int up_value) {
-        this.up_ud_id = up_ud_id;
+    public ud_pt(UmfrageDaten up_ud_id, Partei up_pt_id, int up_value) {
+        this.up_ud = up_ud_id;
         this.up_pt_kürzel = up_pt_id;
         this.up_value = up_value;
     }
 
     @Override
     public String toString() {
-        return "ud_pt{" + "up_ud_id=" + up_ud_id + ", up_pt_name=" + up_pt_kürzel + ", up_value=" + up_value + '}';
+        return "ud_pt{" + "up_ud_id=" + up_ud + ", up_pt_name=" + up_pt_kürzel + ", up_value=" + up_value + '}';
     }
     
     
