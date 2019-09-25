922b8cf94d9604fbb56b3d05217eec2c00f9ed32
20140064 <20140064@IFTS2.htl-stp.if>
2019-09-25 08:50:04 +0200

    Beziehungstabelle wurde für JPA angepasst

--- src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/app/model/ud_pt.java
+++ src/main/java/at/htlstp/projektteam/muhm/testdaten_maven/app/model/ud_pt.java
@@ -10,23 +10,44 @@ package at.htlstp.projektteam.muhm.testdaten_maven.app.model;
  *
  * @author Gabriel Kremsner
  */
-import javax.persistence.OneToOne;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
 
 public class ud_pt {
-    @OneToOne
+    @ManyToOne
+    @Column(name = "ud_pt_ud")
     private UmfrageDaten up_ud;
-    @OneToOne
+    @ManyToOne
+    @Column(name = "ud_pt_kürzel")
     private Partei up_pt_kürzel;
-    private int up_value;
+    @Column(name = "ud_pt_value")
+    private int ud_pt_value;
+
+    public UmfrageDaten getUp_ud() {
+        return up_ud;
+    }
+
+    public void setUp_ud(UmfrageDaten up_ud) {
+        this.up_ud = up_ud;
+    }
+
+    public Partei getUp_pt_kürzel() {
+        return up_pt_kürzel;
+    }
+
+    public void setUp_pt_kürzel(Partei up_pt_kürzel) {
+        this.up_pt_kürzel = up_pt_kürzel;
+    }
 
   
 
-    public int getUp_value() {
-        return up_value;
+    public int getUd_pt_value() {
+        return ud_pt_value;
     }
 
-    public void setUp_value(int up_value) {
-        this.up_value = up_value;
+    public void setUd_pt_value(int ud_pt_value) {
+        this.ud_pt_value = ud_pt_value;
     }
 
     public ud_pt() {
@@ -35,12 +56,45 @@ public class ud_pt {
     public ud_pt(UmfrageDaten up_ud_id, Partei up_pt_id, int up_value) {
         this.up_ud = up_ud_id;
         this.up_pt_kürzel = up_pt_id;
-        this.up_value = up_value;
+        this.ud_pt_value = up_value;
     }
 
     @Override
     public String toString() {
-        return "ud_pt{" + "up_ud_id=" + up_ud + ", up_pt_name=" + up_pt_kürzel + ", up_value=" + up_value + '}';
+        return "ud_pt{" + "up_ud_id=" + up_ud + ", up_pt_name=" + up_pt_kürzel + ", up_value=" + ud_pt_value + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.up_ud);
+        hash = 71 * hash + Objects.hashCode(this.up_pt_kürzel);
+        hash = 71 * hash + this.ud_pt_value;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ud_pt other = (ud_pt) obj;
+        if (this.ud_pt_value != other.ud_pt_value) {
+            return false;
+        }
+        if (!Objects.equals(this.up_ud, other.up_ud)) {
+            return false;
+        }
+        if (!Objects.equals(this.up_pt_kürzel, other.up_pt_kürzel)) {
+            return false;
+        }
+        return true;
     }
     
     
